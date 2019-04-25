7e36228975272a3da946f1510e3743b40394f261
Thiago Callegari <calegari_8@hotmail.com>
2019-04-25 14:46:36 -0300

    Chess layer and printing the board

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,30 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public abstract class ChessPiece extends Piece{
+
+	private Color color;
+	private Integer moveCount;
+	
+	public ChessPiece() {
+	}
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public Integer getMoveCount() {
+		return moveCount;
+	}
+
+	public void setMoveCount(Integer moveCount) {
+		this.moveCount = moveCount;
+	}
+}
