33685a4d594a91661a9b66d863c1de1e5f57982c
Thiago Callegari <calegari_8@hotmail.com>
2019-04-30 00:32:50 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -52,12 +52,15 @@ public class UI {
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
 		System.out.println();
-		printedCapturedPieces(captured);
+		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getcurrentPlayer());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -98,7 +101,7 @@ public class UI {
         System.out.print(" ");
 	}
 	
-	private static void printedCapturedPieces(List<ChessPiece> captured) {
+	private static void printCapturedPieces(List<ChessPiece> captured) {
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
 		System.out.println("Captured pieces:");
